76ad74bbffe3482db9e1c49015068730c50db895
Svalbard <svalbardmalfant@gmail.com>
2022-10-24 15:25:12 +0200

    first commit

--- declarations.js
+++ declarations.js
@@ -4,4 +4,4 @@ const arr = [4, '2'];
 const obj = { str: '', num: 0, bool: false, undef: undefined };
 const nested = { arr: [4,undefined,'2'], obj: {str: '', num: 0, bool: false} };
 
-Object.freeze(nested, arr, obj);
+Object.freeze(nested, nested.arr, nested.obj);
