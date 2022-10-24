2ada38168d3fe5dd7000cf0cc6e2a8c0a349bf2c
Svalbard <svalbardmalfant@gmail.com>
2022-10-24 18:10:40 +0200

    first commit

--- dog-years.js
+++ dog-years.js
@@ -1,3 +1,21 @@
+const planets = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+}
+let year = 31557600;
 
+function dogYears(planet , age){
 
-const dogYears = (planet , age) => age / planet;
+    age = age / year
+    let period = planets[planet]
+    age = age / period
+    age = age * 7
+    return Math.round(age * 100) / 100;
+}
+
