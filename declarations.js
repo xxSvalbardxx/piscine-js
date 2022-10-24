0261d55915ee452c5710573d3b1941bc3ebfc5a5
Svalbard <svalbardmalfant@gmail.com>
2022-10-24 15:24:10 +0200

    first commit

--- declarations.js
+++ declarations.js
@@ -2,6 +2,6 @@
 const escapeStr = '\`\\\/\"\'';
 const arr = [4, '2'];
 const obj = { str: '', num: 0, bool: false, undef: undefined };
-const nested = { arr: [4, undifined, '2'] , obj: {str: '', num: 0, bool: false} };
+const nested = { arr: [4,undefined,'2'], obj: {str: '', num: 0, bool: false} };
 
 Object.freeze(nested, arr, obj);
