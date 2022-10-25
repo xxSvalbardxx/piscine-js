1f553f87c36a201c44d65d0c9189ab3a3063cf15
Svalbard <svalbardmalfant@gmail.com>
2022-10-25 14:27:44 +0200

    first commit

--- method-man.js
+++ method-man.js
@@ -1,5 +1,5 @@
 const words = (str) => str.split(' ');
-const sentences = (arr) => arr.join(' ');
+const sentence = (arr) => arr.join(' ');
 const yell = (str) => str.toUpperCase();
 const whisper = (str) => '*' + str.toLowerCase() + '*';
 const capitalize = (str) => str[0].toUpperCase() + str.substring(1).toLowerCase;
