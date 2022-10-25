b22f2a5af8d8b40fb7a3e2560275c10b1d4aa173
Svalbard <svalbardmalfant@gmail.com>
2022-10-25 15:13:36 +0200

    first commit

---
+++ is.js
@@ -0,0 +1,14 @@
+const is = {
+    num : (x) => typeof x === 'number' ? true : false, 
+    NaN : (x) => typeof x === 'NaN' ? true : false,
+    str : (x) => typeof x === 'string' ? true : false,
+    bool : (x) => typeof x === 'boolean' ? true : false,
+    undef : (x) => typeof x === 'undefined' ? true : false,
+    def : (x) => typeof x !== 'undefined' ? true : false,
+    arr : (x) => typeof x === 'array' ? true : false,
+    obj : (x) => typeof x === 'object' ? true : false,
+    fun : (x) => typeof x === 'function' ? true : false,
+    truthy : (x) => x ? true : false,
+    falsy : (x) => !x ? true : false,
+}
+
