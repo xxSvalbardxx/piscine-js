98ae965c1ccf8ef930473b11d449ee34b7b8ef1b
Svalbard <svalbardmalfant@gmail.com>
2022-10-25 16:01:12 +0200

    first commit

--- is.js
+++ is.js
@@ -6,7 +6,7 @@ is.bool = (x) => typeof x === 'boolean' ? true : false;
 is.undef = (x) => typeof x === 'undefined' ? true : false;
 is.def = (x) => typeof x !== 'undefined' ? true : false;
 is.arr = (x) =>  x instanceof Array ? true : false;
-is.obj = (x) => typeof x === 'object' || x === null  ? true : false;
+is.obj = (x) =>  x instanceof Object ? true : false; // typeof x === 'object' && !Array.isArray(x) && x !== null
 is.fun = (x) => typeof x === 'function' ? true : false;
 is.truthy = (x) => x ? true : false;
 is.falsy = (x) => !x ? true : false;
