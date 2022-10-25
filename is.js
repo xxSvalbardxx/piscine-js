c0dc43dec0f50d7b7dad5dae427b44d937c87a24
Svalbard <svalbardmalfant@gmail.com>
2022-10-25 15:24:32 +0200

    first commit

--- is.js
+++ is.js
@@ -1,13 +1,12 @@
-const is = {
-    num : (x) => typeof x === 'number' ? true : false, 
-    nan : (x) => typeof x === 'NaN' ? true : false,
-    str : (x) => typeof x === 'string' ? true : false,
-    bool : (x) => typeof x === 'boolean' ? true : false,
-    undef : (x) => typeof x === 'undefined' ? true : false,
-    def : (x) => typeof x !== 'undefined' ? true : false,
-    arr : (x) => typeof x === 'array' ? true : false,
-    obj : (x) => typeof x === 'object' ? true : false,
-    fun : (x) => typeof x === 'function' ? true : false,
-    truthy : (x) => x ? true : false,
-    falsy : (x) => !x ? true : false,
-}
+const is = {};
+is.num = (x) => typeof x === 'number' ? true : false; 
+is.nan = (x) => typeof x === 'NaN' ? true : false;
+is.str = (x) => typeof x === 'string' ? true : false;
+is.bool = (x) => typeof x === 'boolean' ? true : false;
+is.undef = (x) => typeof x === 'undefined' ? true : false;
+is.def = (x) => typeof x !== 'undefined' ? true : false;
+is.arr = (x) => typeof x === 'array' ? true : false;
+is.obj = (x) => typeof x === 'object' ? true : false;
+is.fun = (x) => typeof x === 'function' ? true : false;
+is.truthy = (x) => x ? true : false;
+is.falsy = (x) => !x ? true : false;
