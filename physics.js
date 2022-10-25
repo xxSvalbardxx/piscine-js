c226170cfd0f582ddfc5d329c2d680f004f2905f
Svalbard <svalbardmalfant@gmail.com>
2022-10-25 16:24:54 +0200

    first commit

---
+++ physics.js
@@ -0,0 +1,21 @@
+const object = {}
+object.f = 0;
+object.m = 0;
+object.Δv = 0;
+object.Δt = 0;
+object.t = 0;
+object.d = 0;
+
+const getAcceleration = (object) => {
+    if (typeof x === 'object' && !Array.isArray(x) && x !== null){
+        if (object.f && object.m){
+            return object.f / object.m;
+        } else if (object.Δv && object.Δt){
+            return object.Δv / object.Δt;
+        } else if (object.t && object.d){
+            return (object.d * 2)/ (object.t^2);
+        } else {
+            return 'impossible';
+        }
+    }
+}
