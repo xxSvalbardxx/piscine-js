606eaecb6c19ee6d60150b17ca1aa3adcedb86e8
Svalbard <svalbardmalfant@gmail.com>
2022-10-26 17:04:18 +0200

    first commit

--- slicer.js
+++ slicer.js
@@ -1,16 +1,19 @@
-const slice = (arr, start = 0, end = str.length) => {
-  let newArr = "";
-  if (start >= 0) {
-    
-    for (let i = start; i < end; i++) {
-        (newArr += arr[i]);
-    }
-    return newArr;
+const slice = (str, start = 0, end = str.length) => {
+
+    let newStr = "";
+
+    if (start >= 0) {
+        for (let i = start; i < end; i++) {
+            (newStr += str[i]);
+        }
+    return newStr;
   } else if (start < 0) {
-    for (let i = end + start; i < end; i++) {
-        (newArr += arr[i]);
-    }
-    return newArr;
+        for (let j = end + start; j < end; j++) {
+            (newStr += str[i]);
+        }
+    return newStr;
   }
 };
+
 console.log(slice('abcdef', 0, 2));
+console.log(slice('abcdef', 2));
