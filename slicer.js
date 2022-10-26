b9724d02034e6a4a315ae97a697ad39b21f09a7e
Svalbard <svalbardmalfant@gmail.com>
2022-10-26 17:18:33 +0200

    first commit

--- slicer.js
+++ slicer.js
@@ -1,5 +1,5 @@
 const slice = (str, start = 0, end = str.length) => {
-
+if (typeof str === 'string') {
     let newStr = "";
 
     if ((start >= 0) && (end >= 0)) {
@@ -23,7 +23,30 @@ const slice = (str, start = 0, end = str.length) => {
         }
     return newStr;
   }
-
+} else if (str instanceof Array) {
+    let newArr = [];
+    if ((start >= 0) && (end >= 0)) {
+        for (let i = start; i < end; i++) {
+            newArr.push(str[i]);
+        }
+    return newArr;
+  } else if ((start < 0) && (end >= 0)) {
+        for (let j = end + start; j < end; j++) {
+            newArr.push(str[j]);
+        }
+    return newArr;
+  } else if ((start >= 0) && (end < 0)) {
+        for (let k = start; k < str.length + end; k++) {
+            newArr.push(str[k]);
+        }
+    return newArr;
+  } else if ((start < 0) && (end < 0)) {
+        for (let l = str.length + start; l < str.length + end; l++) {
+            newArr.push(str[l]);
+        }
+    return newArr;
+  }
+}
 };
 
 console.log(slice('abcdef', 1, 2));
