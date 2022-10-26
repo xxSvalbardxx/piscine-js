a8000e496493c38291194099fa0360188ef95bad
Svalbard <svalbardmalfant@gmail.com>
2022-10-26 15:49:06 +0200

    first commit

---
+++ index-of.js
@@ -0,0 +1,27 @@
+const indexOf = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+        return i+1;
+        }
+    }
+    return -1;
+};
+
+const lastIndexOf = (arr, value) => {
+    for (let i = arr.length-1; i >= 0; i--) {
+        if (arr[i] === value) {
+        return i;
+        }
+    }
+    return -1;
+};
+const includes = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+        return true;
+        }
+    }
+    return false;
+};
+
+
