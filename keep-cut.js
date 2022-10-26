820637e4248cbe2f506d73d0c0ee87869b41cc94
Svalbard <svalbardmalfant@gmail.com>
2022-10-26 17:33:19 +0200

    first commit

--- keep-cut.js
+++ keep-cut.js
@@ -3,14 +3,19 @@ const cutLast = (str) => str.slice(0,-2);
 const cutFirstLast = (str) => str.slice(2, -2);
 const keepFirst = (str) => str.slice(0, 2);
 const keepLast = (str) => str.slice(-2, str.length);
-const keepFirstLast = (str) => str.slice(0, 2) + str.slice(-2, str.length);
+const keepFirstLast = (str) =>{
+    if (str.length > 4) {
+        str.slice(0, 2) + str.slice(-2, str.length);
+    } else {
+        return str;
+    }
+}; 
 
-/*
-const str = 'abcdef';
+
+const str = 'af';
 console.log('cFirst  '+cutFirst(str));
 console.log('cLast  '+cutLast(str));
 console.log('cFL  '+cutFirstLast(str));
 console.log('kF  '+keepFirst(str));
 console.log('kL  '+keepLast(str));
 console.log('kFL  '+keepFirstLast(str));
-*/
