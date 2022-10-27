44c3d5a74c0dd439249baa075e11d4c5d0ff5b56
Svalbard <svalbardmalfant@gmail.com>
2022-10-27 16:18:48 +0200

    first commit

--- reverser.js
+++ reverser.js
@@ -1,15 +1,23 @@
 const reverse = (str) => {
     let Arr = false;
+    let rslt = "";
+    let result = [];
     if (Array.isArray(str)) {
         Arr = true;
     }
     for (let i = str.length - 1; i >= 0; i--) {
         if (Arr) {
-            str.push(str[i]);
+            result.push(str[i]);
         } else {
-            str += str[i];
+            rslt += str[i];
         }
 
+
     }
-    return str;
+    if (Arr) {
+        return result;
+    }
+    return rslt;
+    
 }
+console.log(reverse("hello"));
