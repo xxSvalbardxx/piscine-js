a96f35cd0c715d77831a9c76578d741ef2a34c43
Svalbard <svalbardmalfant@gmail.com>
2022-10-27 16:24:00 +0200

    first commit

---
+++ unbreakable.js
@@ -0,0 +1,29 @@
+const split = (str, delim) => {
+    let arr = [];
+    let word = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== delim) {
+            word += str[i];
+        } else {
+            arr.push(word);
+            word = "";
+        }
+    }
+    arr.push(word);
+    return arr;
+}
+
+const join = (arr, concatStr) => {
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i];
+        if (i !== arr.length - 1) {
+            str += concatStr;
+        }
+    }
+    return str;
+}
+
+
+console.log(split("hello world", " "));
+console.log(join(["hello", "world"], " "));
