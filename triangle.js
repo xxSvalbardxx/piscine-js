b1ac5c59f4c9207299d444ff848efda569b01d14
Svalbard <svalbardmalfant@gmail.com>
2022-10-31 13:07:33 +0100

    first commit

--- triangle.js
+++ triangle.js
@@ -1,11 +1,15 @@
 function triangle(char , height) {
     var line = "";
+    var rslt = "";
     for (var i = 0; i < height; i++) {
+        
         line += char;
         if (i < height - 1) {
-            line += '\n';
-        }
-        
+            rslt += line + "\n";
+        }else{
+        rslt += line 
+        }        
     }
-    return line;
+    
+    return rslt;
 }
