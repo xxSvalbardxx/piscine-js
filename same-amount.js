15a0d1176e3566a487fbcc7814dfac917d12f48b
Svalbard <svalbardmalfant@gmail.com>
2022-11-01 12:46:44 +0100

    first commit

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,12 @@
 function sameAmount(str, reg1, reg2) {
-    veryfied = typeof str === 'string' && typeof reg1 === 'object' && typeof reg2 === 'object' ? true : false;
+    let veryfied = typeof str === 'string' && typeof reg1 === 'object' && typeof reg2 === 'object' ? true : false;
+    
     const rslt1 = str.match(reg1);
     const rslt2 = str.match(reg2);
     if (rslt1 === null || rslt2 === null) {
+        return false
+    }
+    if (veryfied === false) {
         return false
     }
     return rslt1.length === rslt2.length ? true : false
