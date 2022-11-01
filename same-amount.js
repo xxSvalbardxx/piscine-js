071ce0e4e10d8396157deb66e1c2cbe0f27d0a5d
Svalbard <svalbardmalfant@gmail.com>
2022-11-01 12:54:54 +0100

    first commit

--- same-amount.js
+++ same-amount.js
@@ -1,12 +1,12 @@
 function sameAmount(str, reg1, reg2) {
-    const reg1 = new RegExp(reg1 , "g");
-    const reg2 = new RegExp(reg2 , "g");
+    reg1 = new RegExp(reg1 , "g");
+    reg2 = new RegExp(reg2 , "g");
 
     const rslt1 = str.match(reg1);
     const rslt2 = str.match(reg2);
     if (rslt1 === null || rslt2 === null) {
         return false
     }
-   
+    
     return rslt1.length == rslt2.length ? true : false
 }
