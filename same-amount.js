9d49bb83981f56308e9c8428776753b82edbbfd4
Svalbard <svalbardmalfant@gmail.com>
2022-11-01 12:45:15 +0100

    first commit

--- same-amount.js
+++ same-amount.js
@@ -1,4 +1,5 @@
 function sameAmount(str, reg1, reg2) {
+    veryfied = typeof str === 'string' && typeof reg1 === 'object' && typeof reg2 === 'object' ? true : false;
     const rslt1 = str.match(reg1);
     const rslt2 = str.match(reg2);
     if (rslt1 === null || rslt2 === null) {
