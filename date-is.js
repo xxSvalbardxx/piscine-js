725a0d18b2f0376d9f7c0e1ebd677c339b299a31
Svalbard <svalbardmalfant@gmail.com>
2022-11-01 14:19:32 +0100

    first commit

---
+++ date-is.js
@@ -0,0 +1,16 @@
+function isValid(date) {
+  return date instanceof Date && !isNaN(date);
+}
+function isAfter(date, dateToCompare) {
+  return isValid(date) && isValid(dateToCompare) && date > dateToCompare;
+}
+function isBefore(date, dateToCompare) {
+  return isValid(date) && isValid(dateToCompare) && date < dateToCompare;
+}
+function isFuture(date) {
+  return isValid(date) && date > new Date();
+}
+function isPast(date) {
+  return isValid(date) && date < new Date();
+}
+
