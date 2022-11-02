7c89f1a172913b2025f5af31b0c7cab520f64fd9
Svalbard <svalbardmalfant@gmail.com>
2022-11-02 10:50:49 +0100

    first commit

--- get-some-time.js
+++ get-some-time.js
@@ -1,4 +1,3 @@
-/*
 
 function firstDayWeek(week,year){
     let time = new Date(year)
@@ -8,7 +7,7 @@ function firstDayWeek(week,year){
     }
     let dayPlus = week*7*24
     time.setHours(dayPlus-123)
-//    console.log(time)
+
         function getWeekDay(date) {
             let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
             return days[date.getDay()-1];
@@ -31,57 +30,12 @@ function firstDayWeek(week,year){
             let today = getWeekDay(time)
             if(today === 'Monday'){
                 let res = formattedDate(time)
+                console.log(res)
                 return res
             }
             time.setHours(-24)    
         }
+        console.log(time)
         return time
     }
-    // let date = new Date('0091-06-04')
-    // console.log(date.getDay())
-    // console.log(firstDayWeek(1, '1000'))
-
-    */
-
-function firstDayWeek(week,year){
-    var anne = String(year)
-    if (anne.length === 1) anne = '000' + anne;
-    if (anne.length === 2) anne = '00' + anne;
-    if (anne.length === 3) anne = '0' + anne;
     
-    
-    
-    let time = new Date(anne);
-    
-    let day = (week-1)*7*24
-    time.setHours(day)
-    
-    function formatDate(time) {
-        let month = time.getMonth() + 1;
-        let day = time.getDate()-1;
-        
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-        let rslt = `${day}-${month}-${anne}`;
-        
-        return rslt;
-    }
-    
-    function getWeekDay(date) {
-        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        
-        return days[date.getDay()];
-
-    }
-    
-    for(let i = 0;i<7;i++){
-        let today = getWeekDay(time)
-        if(today === 'Monday'){
-            let res = formatDate(time)
-            console.log(res)        
-            return res
-        }
-    }
-    
-    return time
-}
