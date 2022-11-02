0d3a273f9074c0d81c4c9552f545eef50dae23b3
Svalbard <svalbardmalfant@gmail.com>
2022-11-02 11:16:54 +0100

    first commit

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,9 +1,13 @@
-function countLeapYears(year){
+function countLeapYears(date){
+    let year = date.getFullYear()
     let count = 0
     for(let i = 1;i<year;i++){
         if(i % 4 === 0 && (i % 100 !== 0 || i % 400 === 0)){
             count++
         }
     }
+    console.log(count)
     return count
 }
+
+countLeapYears(new Date('1664-08-09')) // === 403
