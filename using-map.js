d27dcc7c7cab9ba4bc4e29d3384c3c1c42c4b64d
Svalbard <svalbardmalfant@gmail.com>
2022-11-02 12:22:13 +0100

    first commit

---
+++ using-map.js
@@ -0,0 +1,81 @@
+console.log(citiesOnly([
+    {
+      city: 'Los Angeles',
+      temperature: '  101 °F   ',
+    },
+    {
+      city: 'San Francisco',
+      temperature: ' 84 ° F   ',
+    },
+  ]))
+// ['Los Angeles', 'San Francisco']
+function citiesOnly (city) {
+    return city.map(function (city) {
+        return city.city
+    })
+}
+
+
+console.log(upperCasingStates(['alabama', 'new jersey']))
+// ['Alabama', 'New Jersey']
+// must uppercase the first letter of each word
+function upperCasingStates(states) {
+    return states.map(function (states) {
+        words = states.split(' ')
+        for (var i = 0; i < words.length; i++) {
+            words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+        }
+        return words.join(' ')
+    })
+}
+
+
+console.log(fahrenheitToCelsius(['68°F', '59°F', '25°F']));
+// [20, 15, -4]
+
+function fahrenheitToCelsius (temp) {
+    return temp.map(function (temp) {
+        celsius = Math.round((parseInt(temp) - 32) * 5 / 9)
+        return (celsius + '°C')
+    })
+}
+
+
+console.log(trimTemp([{ city: 'Los Angeles', temperature: '  101 °F   ' },
+{ city: 'San Francisco', temperature: ' 84 ° F   ' },
+]))
+/* -> [
+{ city: 'Los Angeles', temperature: '101°F' },
+{ city: 'San Francisco', temperature: '84°F' },
+] */
+
+function trimTemp(obj) {
+    return obj.map(function (obj) {
+        t = obj.temperature.replaceAll(' ', '')
+        return t
+    })
+}
+
+console.log(tempForecasts([
+    {
+      city: 'Pasadena',
+      temperature: ' 101 °F',
+      state: 'new jersey',
+      region: 'West',
+    },
+  ])) // -> ['38°Celsius in Pasadena, New Jersey']
+
+function tempForecasts(obj) {
+    return obj.map(function (obj) {
+        t = obj.temperature.replaceAll(' ', '')
+        celsius = Math.round((parseInt(t) - 32) * 5 / 9)
+        words = obj.state.split(' ')
+        for (var i = 0; i < words.length; i++) {
+            words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+        }
+        state = words.join(' ')
+        return (celsius + '°Celsius in ' + obj.city + ', ' + state)
+    })
+}
+
+   
