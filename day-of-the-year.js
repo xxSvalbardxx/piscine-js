1693b4dc175b5bd99300cea92c59dcab4860f6c7
Svalbard <svalbardmalfant@gmail.com>
2022-11-02 11:24:52 +0100

    first commit

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,7 +1,15 @@
+// dayOfTheYear(new Date('0001-01-01')) === 1
+
 function dayOfTheYear(date) {
-  let rslt = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24)
-  console.log(rslt)
-  return rslt;
+    let year = date.getFullYear();
+    let days = 0;
+    for (let i = 1; i < date.getMonth() + 1; i++) {
+        days += new Date(year, i, 0).getDate();
+    }
+    days += date.getDate();
+    console.log(days);
+    return days;
+
 }
 
-dayOfTheYear(new Date('2020-10-01')) // === 1
+dayOfTheYear(new Date('0001-01-01')) === 1
