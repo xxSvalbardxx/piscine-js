b6c407293b7cf6fb7f46416057b086b4793e3161
Svalbard <svalbardmalfant@gmail.com>
2022-11-03 16:06:49 +0100

    first commit

---
+++ reduce.js
@@ -0,0 +1,23 @@
+function fold(arr, func, accumulateur){
+    newArr = [];
+    for (let i = 0 ; i < arr.lenght ; i++) {
+        newArr.push(func(arr[i], accumulateur));
+    }
+    return newArr;
+}
+
+function foldRight(arr, func, accumulateur){
+    newArr = [];
+    for (let i = arr.lenght-1 ; i > 0 ; i--) {
+        newArr.push(func(arr[i], accumulateur));
+    }
+    return newArr;
+}
+
+function reduce(arr, func, accumulateur){
+    newArr = [];
+    for (let i = 0 ; i < arr.lenght ; i++) {
+        newArr.push(func(arr[i], accumulateur));
+    }
+    return newArr;
+}
