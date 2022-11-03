58a45699cc1b63d6e1fedf867f052cef80446784
Svalbard <svalbardmalfant@gmail.com>
2022-11-03 16:51:42 +0100

    first commit

--- reduce.js
+++ reduce.js
@@ -13,24 +13,25 @@ function foldRight(arr = [], func = (val, index, arr), acc=0){
     return rslt;
 }
 function reduce(arr = [], func = (val, index, arr), acc=0){
-    let rslt = acc;
+    let newArr = [];
     if (arr.length < 2){
         return Error("Array is not long enough");
     }
     for (let i = 0; i < arr.length; i++) {
-        acc = func(acc, arr[i], i, arr); // func(accumulateur, element, index, array) => func(0, 1, 0, [1,2,3]) => func(1, 2, 1, [1,2,3]) => func(3, 3, 2, [1,2,3]) => func(6, undefined, 3, [1,2,3])
-
+        newArr.push(func(acc, arr[i], i, arr)); // func(accumulateur, element, index, array) => func(0, 1, 0, [1,2,3]) => func(1, 2, 1, [1,2,3]) => func(3, 3, 2, [1,2,3]) => func(6, undefined, 3, [1,2,3])
+        acc = newArr[i];
     }
-    return rslt;
+    return newArr[newArr.length - 1];
 }
 function reduceRight(arr = [], func = (val, index, arr), acc=0){
-    let rslt = acc;
+    let newArr = [];
     if (arr.length < 2){
         return Error("Array is not long enough");
     }
     for (let i = arr.length - 1; i >= 0; i--) {
-        acc = func(acc, arr[i], i, arr);
+        newArray(func(acc, arr[i], i, arr));
+        acc = newArr[i];
         
     }
-    return rslt;
+    return newArr[newArr.length - 1];
 }
