5683f96e8dfb5ee5d0fcc4a4b6b1f2745a72f8f2
Svalbard <svalbardmalfant@gmail.com>
2022-11-03 17:08:45 +0100

    first commit

--- reduce.js
+++ reduce.js
@@ -12,23 +12,22 @@ function foldRight(arr = [], func = (val, index, arr), acc=0){
     }
     return rslt;
 }
-function reduce(arr = [], func = (val, index, arr)){
+function reduce(arr, func ){
     if (arr.length < 2){
         return Error("Array is not long enough");
     }
-
     let rslt = arr[0];
-    for (let i = 0; i < arr.length- 1; i++) {
+    for (let i = 1; i < arr.length- 1; i++) {
         rslt = func(rslt, arr[i]);
     }
     return rslt;
 }
-function reduceRight(arr = [], func = (val, index, arr)){
+function reduceRight(arr , func){
     if (arr.length < 2){
         return Error("Array is not long enough");
     }
     let rslt = arr[arr.length - 1];
-    for (let i = arr.length - 1; i >= 0; i--) {
+    for (let i = arr.length - 2; i >= 0; i--) {
         rslt = func(rslt, arr[i]);
     }
     return rslt;    
