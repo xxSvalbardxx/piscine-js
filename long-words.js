53aa8f6b33f20570fcc0cd684d7a7af821b3060f
Svalbard <svalbardmalfant@gmail.com>
2022-11-03 15:07:36 +0100

    first commit

--- long-words.js
+++ long-words.js
@@ -1,34 +1,34 @@
-function longWords(words) {
+function longWords(words, acc=0) {
     let shortWords = 0;   
     words.map(function(word) {
         if (word.length < 5){
             shortWords+=1;
         }  
-    });
+    },acc);
     if (shortWords !== 0){
         return false
     }
     return true
 }
-function oneLongWord(words) {
+function oneLongWord(words, acc=0) {
     let longWords = 0;
     words.map(function(word) {
         if (word.length >= 10){
             longWords+=1;
         }  
-    });
+    },acc);
     if (longWords >= 1){
         return true
     }
     return false
 }
-function noLongWords(words) {
+function noLongWords(words, acc=0) {
     let longWords = 0;
     words.map(function(word) {
         if (word.length >= 7){
             longWords+=1;
         }  
-    });
+    },acc);
     if (longWords === 0){
         return true
     }
