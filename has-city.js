ee3c3a6a7955057fe5f6b488239923ca3cf7a1b8
Svalbard <svalbardmalfant@gmail.com>
2022-11-04 12:12:17 +0100

    first commit

--- has-city.js
+++ has-city.js
@@ -1,9 +1,11 @@
 function hasCity(country, cities = []) {
     return cities.some(city => {
-       if (country[cities]=== (city)){
-              return `${city} is a city from ${country.name}`;
-       }
-       return `${city} is not a city from ${country.name}`;
+        for (let i = 0; i < country.length; i++) {
+            if (country[cities]=== (city)){
+                return `${city} is a city from ${country.name}`;
+            }
+            return `${city} is not a city from ${country.name}`;
+        }
     });
 }
 
