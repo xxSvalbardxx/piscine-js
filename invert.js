67a0e2ab34e273d9cc89ded0195caef9f1da5a5e
Svalbard <svalbardmalfant@gmail.com>
2022-11-08 16:04:36 +0100

    lol

--- invert.js
+++ invert.js
@@ -1,7 +1,9 @@
 function invert(obj) {
-    var inverted = {};
-    for (var key in obj) {
-        inverted[obj[key]] = key;
-    }
-    return inverted;
+    let obj2 = {}
+    let keys = Object.keys(obj)
+    keys.forEach(key => {
+        let val = obj[key]
+        obj2[val] = key
+    })
+    return obj2
 }
