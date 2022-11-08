15ded156883cd445db003a102d186a92c4a2c7c7
Svalbard <svalbardmalfant@gmail.com>
2022-11-08 16:32:16 +0100

    lol

--- pick-omit.js
+++ pick-omit.js
@@ -1,27 +1,37 @@
-
 // pick: return an object contains only those keys which appear in the string or array of strings.
-function pick(obj, str){
-    let obj2 = {}
-    let keys = Object.keys(obj)
-    keys.forEach(key => {
-        if (str == key){
-            obj2[key] = obj[key]
-        }
-    })
-    return obj2
+function pick(obj, str) {
+  if (typeof str == "string") {
+    str = [str];
+  }
+  let obj2 = {};
+  let keys = Object.keys(obj);
+
+  for (let i = 0; i < str.length; i++) {
+    keys.forEach((key) => {
+      if (str[i] == key) {
+        obj2[key] = obj[key];
+      }
+    });
+    return obj2;
+  }
 }
 
 // omit: return an object contains only those keys which do not appear in the string or array of strings.
-function omit(obj, str){
-    let obj2 = {}
-    let keys = Object.keys(obj)
-    keys.forEach(key => {
-        if (str != key){
-            obj2[key] = obj[key]
-        }
-    })
-    return obj2
+function omit(obj, str) {
+  if (typeof str == "string") {
+    str = [str];
+  }
+  let obj2 = {};
+  let keys = Object.keys(obj);
+  for (let i = 0; i < str.length; i++) {
+    keys.forEach((key) => {
+      if (str[i] != key) {
+        obj2[key] = obj[key];
+      }
+    });
+    return obj2;
+  }
 }
 
- console.log(pick({age:1, agehg:2, c:3}, 'agehg')) // {a:1}
- console.log(omit({a:1, aa:2, c:3}, ['a'])) // {b:2}
+console.log(pick({ age: 1, agehg: 2, c: 3 }, "agehg")); // {a:1}
+console.log(omit({ a: 1, aa: 2, c: 3 }, ["a"])); // {b:2}
