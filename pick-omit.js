ed2bb6f46c15553af4f58219b28236a5b6750787
Svalbard <svalbardmalfant@gmail.com>
2022-11-08 16:17:49 +0100

    lol

--- pick-omit.js
+++ pick-omit.js
@@ -4,7 +4,7 @@ function pick(obj, str){
     let obj2 = {}
     let keys = Object.keys(obj)
     keys.forEach(key => {
-        if (str.includes(key)){
+        if (str.match(key)){
             obj2[key] = obj[key]
         }
     })
@@ -16,12 +16,12 @@ function omit(obj, str){
     let obj2 = {}
     let keys = Object.keys(obj)
     keys.forEach(key => {
-        if (!str.includes(key)){
+        if (!str.match(key)){
             obj2[key] = obj[key]
         }
     })
     return obj2
 }
 
-// console.log(pick({a:1, b:2, c:3}, 'a')) // {a:1}
+ console.log(pick({a:1, aa:2, c:3}, 'a')) // {a:1}
 // console.log(omit({a:1, b:2, c:3}, ['a', 'c'])) // {b:2}
