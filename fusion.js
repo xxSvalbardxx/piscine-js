5f59b44f3d2c098e51c5a1dc2b12214d1841f56f
Svalbard <svalbardmalfant@gmail.com>
2022-11-09 11:01:36 +0100

    lol

--- fusion.js
+++ fusion.js
@@ -63,21 +63,21 @@ function fusion(obj1, obj2) {
     return result;
 }
 /*
-fusion({ arr: [1, "2"] }, { arr: [2] }); // -> { arr: [ 1, '2', 2 ] } 
+console.log(fusion({ arr: [1, "2"] }, { arr: [2] })); // -> { arr: [ 1, '2', 2 ] };
 console.log('---------------------');
-fusion({ arr: [], arr1: [5] },{ arr: [10, 3], arr1: [15, 3], arr2: ["7", "1"] }); // ->{ arr: [ 10, 3 ], arr1: [ 5, 15, 3 ], arr2: [ '7', '1' ] }
+console.log(fusion({ arr: [], arr1: [5] },{ arr: [10, 3], arr1: [15, 3], arr2: ["7", "1"] })); // ->{ arr: [ 10, 3 ], arr1: [ 5, 15, 3 ], arr2: [ '7', '1' ] }
 console.log("------------------------------------------------------------");
-fusion({ str: "salem" }, { str: "alem" }); // -> { str: 'salem alem' }
+console.log(fusion({ str: "salem" }, { str: "alem" })); // -> { str: 'salem alem' };
 console.log('---------------------');
-fusion({ str: "salem" }, { str: "" }); // -> { str: 'salem ' }
+console.log(fusion({ str: "salem" }, { str: "" })); // -> { str: 'salem ' });
 console.log("------------------------------------------------------------");
-fusion({ a: 10, b: 8, c: 1 }, { a: 10, b: 2 }); // -> { a: 20, b: 10, c: 1 }
+console.log(fusion({ a: 10, b: 8, c: 1 }, { a: 10, b: 2 }); // -> { a: 20, b: 10, c: 1 }
 console.log("------------------------------------------------------------");
-fusion({ a: 1, b: { c: "Salem" } }, { a: 10, x: [], b: { c: "alem" } }); // -> { a: 11, x: [], b: { c: 'Salem alem' } }
+console.log(fusion({ a: 1, b: { c: "Salem" } }, { a: 10, x: [], b: { c: "alem" } })); // -> { a: 11, x: [], b: { c: 'Salem alem' } }
 console.log('---------------------');
-console.log(fusion( { a: { b: [3, 2], c: { d: 8 } } },{ a: { b: [0, 3, 1], c: { d: 3 } } })); // -> { a: { b: [ 3, 2, 0, 3, 1 ], c: { d: 11 } } }
+console.log(fusion( { a: { b: [3, 2], c: { d: 8 } } },{ a: { b: [0, 3, 1], c: { d: 3 } } }))); // -> { a: { b: [ 3, 2, 0, 3, 1 ], c: { d: 11 } } }
 console.log("------------------------------------------------------------");
-fusion({ a: "hello", b: [] }, { a: 4 }); // -> { a: 4, b: [] }
+console.log(fusion({ a: "hello", b: [] }, { a: 4 })); // -> { a: 4, b: [] }
 */
 
 
---
+++ manipulate-values.js
@@ -0,0 +1,37 @@
+
+// small database with nutrition facts, per 100 grams
+// In this exercise this is used for testing purposes only
+// prettier-ignore
+const nutritionDB = {
+    tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+    vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+    oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+    onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+    garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+    paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+    sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+    orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
+}
+
+function filterValues(obj, func){
+    return Object.fromEntries(Object.entries(obj).filter(([key, value]) => func(value)));
+    
+}
+
+function mapValues(){
+
+}
+function reduceValues(){
+
+}
+
+const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
+
+console.log(filterValues(nutrients, (nutrient) => nutrient <= 12))
+// output: { carbohydrates: 12, fat: 5 }
+
+console.log(mapValues(nutrients, (v) => v+1))
+// output: { carbohydrates: 13, protein: 21, fat: 6 }
+
+console.log(reduceValues(nutrients, (acc, cr) => acc + cr))
+// output: 37
